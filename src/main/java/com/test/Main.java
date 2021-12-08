31639bededcb39587acf24081aa6a72ecfacc8d9
xiehongwei <xhw529609672>
2021-12-08 23:17:52 +0800

    add:readme and 打包可运行jar

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author xiehongwei
+ * @date 2021/12/8 11:11 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test build jar");
+    }
+}
