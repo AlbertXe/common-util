2d3374b8ddfa629ea6bd77defd6352bcbf585156
AlbertXe <563000409@qq.com>
2022-06-16 21:53:54 +0800

    springboot阅读

---
+++ MyApp/src/main/java/com/MyApp.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: AlbertXe
+ * @create: 2022-06-16 21:50
+ */
+@SpringBootApplication
+public class MyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApp.class, args);
+    }
+}
