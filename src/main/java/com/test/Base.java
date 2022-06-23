437c31f317bb1c63576814b96c470a2584f16088
AlbertXe <563000409@qq.com>
2022-06-23 19:44:35 +0800

    test

--- src/main/java/com/test/Base.java
+++ src/main/java/com/test/Base.java
@@ -1,18 +1,17 @@
 package com.test;
 
 public class Base {
-    public static final Integer FIELD_0 = 0;
-    public static final String FIELD_A = "base_field_a";
-    public static String FIELD_B = "base_field_b";
-    public String FIELD_C = "base_field_c";
-
+    public static final Integer FIELD_0 = 1;
+    public static final String FIELD_A = "field_a";
+    public static String FIELD_B = "field_b";
+    public String FIELD_C = "field_c";
 
     static {
-        System.out.println("base_cinit");
+        System.out.println("static{}");
     }
 
     public Base() {
-        System.out.println("base_init");
+        System.out.println("base constructor");
     }
 
     public static void methodA() {
@@ -30,11 +29,11 @@ class Extended extends Base {
     public String FIELD_C = "extended_field_c";
 
     static {
-        System.out.println("extended_cinit");
+        System.out.println("extended static{}");
     }
 
     public Extended() {
-        System.out.println("extended_init");
+        System.out.println("extended constructor");
     }
 
     public static void methodA() {
@@ -47,24 +46,23 @@ class Extended extends Base {
 }
 
 class Test {
-
     public static void main(String[] args) {
-//        System.out.println(Base.FIELD_0);                                                                                           //base_cinit 0
-//        System.out.println(Base.FIELD_A);                                                                                         //base_field_a
-//        System.out.println(Base.FIELD_B);                                                                                         //base_cinit base_field_b
-//        Base.methodA();                                                                                                           //base_cinit base_method_a
-//        new Base().methodB();                                                                                                     //base_cinit base_init base_method_b
+        System.out.println(Base.FIELD_0);
+//        System.out.println(Base.FIELD_A);
+//        System.out.println(Base.FIELD_B);
+//        Base.methodA();
+//        new Base().methodB();
 
-//        System.out.println(Extended.FIELD_A);                                                                                     //extended_field_a
-//        System.out.println(Extended.FIELD_B);                                                                                     //base_cinit extended_cinit extended_field_b
-        Extended.methodA();                                                                                                       //base_cinit extended_cinit extended_method_a
-        //new Extended().methodB();                                                                                                 //base_cinit extended_cinit base_init extended_init extended_method_b
-
-        //Base base = new Extended();
-        //System.out.println(base.FIELD_A);                                                                                         //base_cinit extended_cinit base_init extended_init base_field_a
-        //System.out.println(base.FIELD_B);                                                                                         //base_cinit extended_cinit base_init extended_init base_field_b
-        //System.out.println(base.FIELD_C);                                                                                         //base_cinit extended_cinit base_init extended_init base_field_c
-        //base.methodA();                                                                                                           //base_field_c extended_cinit base_init extended_init base_method_a
-        //base.methodB();                                                                                                           //base_cinit extended_cinit base_init extended_init extended_method_b
+//        System.out.println(Extended.FIELD_A);
+//        System.out.println(Extended.FIELD_B);
+//        Extended.methodA();
+//        new Extended().methodB();
+//
+//        Base base = new Extended();
+//        System.out.println(base.FIELD_A);
+//        System.out.println(base.FIELD_B);
+//        System.out.println(base.FIELD_C);
+//        base.methodA();
+//        base.methodB();
     }
 }
