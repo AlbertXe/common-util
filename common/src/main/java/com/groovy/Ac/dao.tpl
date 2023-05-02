c803be26c507f9b0844e42daf2853657c094bbd5
AlbertXe <563000409@qq.com>
2023-05-03 07:56:45 +0800

    groovy-acEvntTbl

--- common/src/main/java/com/groovy/Ac/dao.tpl
+++ common/src/main/java/com/groovy/Ac/dao.tpl
@@ -1,4 +1,10 @@
+package ${basePackage};
 
-public class $id$Dao{
-
+public class ${id}Dao{
+    <% fieldList.each { it->%>
+    private String ${it.id};<%}%>
+    <% fieldList.each { it->%>
+        public void get${it.id}(){
+        }
+        <%}%>
 }
--- common/src/main/java/com/groovy/Ac/tables.groovy
+++ common/src/main/java/com/groovy/Ac/tables.groovy
@@ -2,31 +2,64 @@ package com.groovy.Ac
 
 import groovy.text.GStringTemplateEngine
 
-tables = getTables("D:\\idea_study2\\common-util\\common\\src\\main\\java\\com\\groovy\\Ac\\Ac.tables.xml")
-
-failures = generateAcTables()
-
 def bindings = [:]
+tables = getTables("/Users/xiehongwei/IdeaProjects/common-util/common/src/main/java/com/groovy/Ac/Ac.tables.xml",bindings)
 
-def generateAcTables() {
+failures = generateAcTables('AcEvntTbl',bindings)
+
+
+
+def generateAcTables(tableId,bindings) {
+    def table = bindings.tableMap.get(tableId)
+    println("generateAcTables table="+table)
+
+    table.basePackage="com.groovy.Ac"
+    wrireFile("/Users/xiehongwei/IdeaProjects/common-util/common/src/main/java/com/groovy/Ac/dao.tpl",table)
+
 
 }
 
-def getTables(String tableFile) {
+def getTables(tableFile,bindings) {
     def file = new File(tableFile)
-    def tables = new XmlParser().parse(file).children()
+    def tables = new XmlParser().parse(file)
 
-    println(tables)
-    tables.each {tbl->
-        println(tbl)
+    println("tables=" + tables)
+
+    def tableMap = [:]
+    tables.each { tbl ->
+        println("tbl=" + tbl)
+        def table = [:]
+        table.id = tbl.@id
+        table.name = tbl.@name
+
+        def fieldMap = [:]
+        def fieldList = []
+        def fields = tbl.columns.field;
+        fields.each { fld ->
+            def field = [:]
+            field.id=fld.@id
+            field.name=fld.@name
+            field.user=fld.@user
+
+            fieldMap.put(field.id,field)
+            fieldList.add(field)
+        }
+        table.fieldMap = fieldMap
+        table.fieldList = fieldList
+                tableMap.put(table.id,table)
     }
-    wrireFile("dao.tpl",bindings)
+    bindings.tableMap=tableMap
 }
 
-def wrireFile(fileName,bindings) {
-    def f = new File(fileName)
+def wrireFile(templateFile, binding) {
+    def f = new File(templateFile)
     def engine = new GStringTemplateEngine()
-    def template = engine.createTemplate(f).make(bindings)
-    println(template.toString())
+    def template = engine.createTemplate(f).make(binding)
+
+    def outFile = "/Users/xiehongwei/IdeaProjects/common-util/common/src/main/java/com/groovy/Ac/AcEvntTblDao.java"
+    def writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outFile)))
+    writer.write(template.toString())
+    writer.flush()
+    writer.close()
 
 }
