7c68ac13bf5059b48dc3e3d13b7179e377b4b6a2
AlbertXe <563000409@qq.com>
2024-05-29 22:17:07 +0800

    javaAgent ByteBuddy操作字节码编程

--- JavaAgentDemo/src/main/java/com/ApiTest.java
+++ JavaAgentDemo/src/main/java/com/ApiTest.java
@@ -2,13 +2,14 @@ package com;
 
 public class ApiTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ApiTest apiTest = new ApiTest();
         apiTest.echoHi();
     }
 
-    private void echoHi(){
+    private void echoHi() throws InterruptedException {
         System.out.println("hi agent");
+        Thread.sleep((long) (Math.random() * 500));
     }
 
 }
--- JavaAgentDemo/src/main/java/com/MyAgent.java
+++ JavaAgentDemo/src/main/java/com/MyAgent.java
@@ -1,5 +1,13 @@
 package com;
 
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
+
 import java.lang.instrument.Instrumentation;
 
 /**
@@ -9,10 +17,56 @@ import java.lang.instrument.Instrumentation;
  */
 public class MyAgent {
     //JVM 首先尝试在代理类上调用以下方法
+//    public static void premain(String agentArgs, Instrumentation inst) {
+//        System.out.println("this is my agent：" + agentArgs);
+//        MyMonitorTransformer monitor = new MyMonitorTransformer();
+//        inst.addTransformer(monitor);
+//    }
+
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("this is my agent：" + agentArgs);
-        MyMonitorTransformer monitor = new MyMonitorTransformer();
-        inst.addTransformer(monitor);
+
+        AgentBuilder.Transformer transformer = (builder, typeDescription, classLoader, javaModule) -> {
+            return builder
+                    .method(ElementMatchers.any()) // 拦截任意方法
+                    .intercept(MethodDelegation.to(MethodCostTime.class)); // 委托
+        };
+
+        AgentBuilder.Listener listener = new AgentBuilder.Listener() {
+            @Override
+            public void onDiscovery(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
+
+            }
+
+            @Override
+            public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b, DynamicType dynamicType) {
+
+            }
+
+            @Override
+            public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b) {
+
+            }
+
+            @Override
+            public void onError(String s, ClassLoader classLoader, JavaModule javaModule, boolean b, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
+
+            }
+
+        };
+
+        new AgentBuilder
+                .Default()
+//                .type(ElementMatchers.nameStartsWith("")) // 指定需要拦截的类
+                .type(ElementMatchers.any()) // 指定需要拦截的类
+                .transform(transformer)
+                .with(listener)
+                .installOn(inst);
     }
 
     //如果代理类没有实现上面的方法，那么 JVM 将尝试调用该方法
