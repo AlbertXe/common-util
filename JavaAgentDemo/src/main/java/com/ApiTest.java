f7b3da66cde667b771226d2fe472f1ab3378839e
AlbertXe <563000409@qq.com>
2024-05-29 22:43:27 +0800

    javaAgent JVM与GC内存信息

--- JavaAgentDemo/src/main/java/com/ApiTest.java
+++ JavaAgentDemo/src/main/java/com/ApiTest.java
@@ -1,10 +1,22 @@
 package com;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ApiTest {
 
-    public static void main(String[] args) throws InterruptedException {
-        ApiTest apiTest = new ApiTest();
-        apiTest.echoHi();
+//    public static void main(String[] args) throws InterruptedException {
+//        ApiTest apiTest = new ApiTest();
+//        apiTest.echoHi();
+//    }
+
+    public static void main(String[] args) {
+        while (true) {
+            List<Object> list = new LinkedList<>();
+            list.add("嗨！JavaAgent");
+            list.add("嗨！JavaAgent");
+            list.add("嗨！JavaAgent");
+        }
     }
 
     private void echoHi() throws InterruptedException {
--- JavaAgentDemo/src/main/java/com/MyAgent.java
+++ JavaAgentDemo/src/main/java/com/MyAgent.java
@@ -1,14 +1,9 @@
 package com;
 
 
-import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.matcher.ElementMatchers;
-import net.bytebuddy.utility.JavaModule;
-
 import java.lang.instrument.Instrumentation;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description:todo
@@ -23,50 +18,64 @@ public class MyAgent {
 //        inst.addTransformer(monitor);
 //    }
 
+//    public static void premain(String agentArgs, Instrumentation inst) {
+//        System.out.println("this is my agent：" + agentArgs);
+//
+//        AgentBuilder.Transformer transformer = (builder, typeDescription, classLoader, javaModule) -> {
+//            return builder
+//                    .method(ElementMatchers.any()) // 拦截任意方法
+//                    .intercept(MethodDelegation.to(MethodCostTime.class)); // 委托
+//        };
+//
+//        AgentBuilder.Listener listener = new AgentBuilder.Listener() {
+//            @Override
+//            public void onDiscovery(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
+//
+//            }
+//
+//            @Override
+//            public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b, DynamicType dynamicType) {
+//
+//            }
+//
+//            @Override
+//            public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b) {
+//
+//            }
+//
+//            @Override
+//            public void onError(String s, ClassLoader classLoader, JavaModule javaModule, boolean b, Throwable throwable) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
+//
+//            }
+//
+//        };
+//
+//        new AgentBuilder
+//                .Default()
+////                .type(ElementMatchers.nameStartsWith("")) // 指定需要拦截的类
+//                .type(ElementMatchers.any()) // 指定需要拦截的类
+//                .transform(transformer)
+//                .with(listener)
+//                .installOn(inst);
+//    }
+
+    //JVM与内存信息
+
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("this is my agent：" + agentArgs);
 
-        AgentBuilder.Transformer transformer = (builder, typeDescription, classLoader, javaModule) -> {
-            return builder
-                    .method(ElementMatchers.any()) // 拦截任意方法
-                    .intercept(MethodDelegation.to(MethodCostTime.class)); // 委托
-        };
-
-        AgentBuilder.Listener listener = new AgentBuilder.Listener() {
-            @Override
-            public void onDiscovery(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
-
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                JvmStack.printMemoryInfo();
+                JvmStack.printGCInfo();
+                System.out.println("===================================================================================================");
             }
-
-            @Override
-            public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b, DynamicType dynamicType) {
-
-            }
-
-            @Override
-            public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule, boolean b) {
-
-            }
-
-            @Override
-            public void onError(String s, ClassLoader classLoader, JavaModule javaModule, boolean b, Throwable throwable) {
-
-            }
-
-            @Override
-            public void onComplete(String s, ClassLoader classLoader, JavaModule javaModule, boolean b) {
-
-            }
-
-        };
-
-        new AgentBuilder
-                .Default()
-//                .type(ElementMatchers.nameStartsWith("")) // 指定需要拦截的类
-                .type(ElementMatchers.any()) // 指定需要拦截的类
-                .transform(transformer)
-                .with(listener)
-                .installOn(inst);
+        }, 0, 5000, TimeUnit.MILLISECONDS);
     }
 
     //如果代理类没有实现上面的方法，那么 JVM 将尝试调用该方法
